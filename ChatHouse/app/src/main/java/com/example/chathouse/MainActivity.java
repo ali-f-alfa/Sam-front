5e5f442a342134a3857ee3c45df93e1a1753b98f
ali <ali.farahat78@gmail.com>
2021-03-09 16:24:35 +0330

    starting

---
+++ ChatHouse/app/src/main/java/com/example/chathouse/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.chathouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setVisibility(View.INVISIBLE);
+
+        Button mButton = findViewById(R.id.btn1);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                textView1.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
