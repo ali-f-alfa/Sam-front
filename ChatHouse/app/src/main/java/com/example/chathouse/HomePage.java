2331d627352472fdd92926a7cd8d9db490eaccb5
MELIKA AHMADI RANJBAR <melika.ahmadi2000@yahoo.com>
2021-04-23 16:45:24 +0430

    ModalStarted

--- ChatHouse/app/src/main/java/com/example/chathouse/HomePage.java
+++ ChatHouse/app/src/main/java/com/example/chathouse/HomePage.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.chathouse.API.ChatHouseAPI;
@@ -44,6 +45,8 @@ public class HomePage extends AppCompatActivity {
 
     private Button StartRoom;
     private ProgressBar loading;
+    private Call<GetRoomViewModel> CreateRoom;
+    private ChatHouseAPI APIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,43 +84,15 @@ public class HomePage extends AppCompatActivity {
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory((GsonConverterFactory.create(gson)))
                 .build();
-        ChatHouseAPI APIS = retrofit.create(ChatHouseAPI.class);
+        APIS = retrofit.create(ChatHouseAPI.class);
 
-        CreateRoomViewModel Room = RoomModel("test", "test", null, null);
-        Call<GetRoomViewModel> CreateRoom = APIS.CreateRoom(Room);
+
         StartRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Open(v);
 
-//                loading.setVisibility(View.VISIBLE);
-//                CreateRoom.enqueue(new Callback<GetRoomViewModel>() {
-//                    @Override
-//                    public void onResponse(Call<GetRoomViewModel> call, Response<GetRoomViewModel> response) {
-//                        if(!response.isSuccessful()){
-//                            try {
-//                                loading.setVisibility(View.INVISIBLE);
-//                                System.out.println("1" + response.errorBody().string());
-//                                System.out.println("1" + response.code());
-//                                System.out.println(response.errorBody().string());
-//                            } catch (IOException e) {
-//                                loading.setVisibility(View.INVISIBLE);
-//                                System.out.println("2" + response.errorBody().toString());
-//
-//                                e.printStackTrace();
-//                            }
-//                            return;
-//                        }
-//                        loading.setVisibility(View.INVISIBLE);
-//                        System.out.println("Room just Created Okay" + response.body());
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<GetRoomViewModel> call, Throwable t) {
-//                        loading.setVisibility(View.INVISIBLE);
-//                        Toast.makeText(HomePage.this, "Request failed", Toast.LENGTH_LONG).show();
-//                    }
-//                });
+
             }
         });
     }
@@ -139,12 +114,53 @@ public class HomePage extends AppCompatActivity {
     }
 
     public void Open(View view) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(HomePage.this);
-        ViewGroup viewGroup = findViewById(android.R.id.content);
-        View dialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.dialog_room_creation, viewGroup, false);
-        builder.setView(dialogView);
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
 
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_room_creation);
+
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.Create);
+        // if button is clicked, close the custom dialog
+        EditText name = (EditText)dialog.findViewById(R.id.editNameRoom);
+        EditText description = (EditText)dialog.findViewById(R.id.editDesRoom);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateRoomViewModel Room = RoomModel(name.getText().toString(), description.getText().toString(), null, null);
+                CreateRoom = APIS.CreateRoom(Room);
+                loading.setVisibility(View.VISIBLE);
+                CreateRoom.enqueue(new Callback<GetRoomViewModel>() {
+                    @Override
+                    public void onResponse(Call<GetRoomViewModel> call, Response<GetRoomViewModel> response) {
+                        if(!response.isSuccessful()){
+                            try {
+                                loading.setVisibility(View.INVISIBLE);
+                                System.out.println("1" + response.errorBody().string());
+                                System.out.println("1" + response.code());
+                                System.out.println(response.errorBody().string());
+                            } catch (IOException e) {
+                                loading.setVisibility(View.INVISIBLE);
+                                System.out.println("2" + response.errorBody().toString());
+
+                                e.printStackTrace();
+                            }
+                            return;
+                        }
+                        loading.setVisibility(View.INVISIBLE);
+                        System.out.println("Room just Created Okay" + response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<GetRoomViewModel> call, Throwable t) {
+                        loading.setVisibility(View.INVISIBLE);
+                        Toast.makeText(HomePage.this, "Request failed", Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
+
 }
