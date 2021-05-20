0d2b9aec589b02f50deb1769b47e90dc33e8a781
ali <ali.farahat78@gmail.com>
2021-05-20 13:13:07 +0430

    unit test added

---
+++ ChatHouse/app/src/test/java/com/example/chathouse/Pages/HomePageTest1.java
@@ -0,0 +1,14 @@
+package com.example.chathouse.Pages;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+public class HomePageTest1 {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
