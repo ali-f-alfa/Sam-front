b6b4f5c129e94a2816815c5c88d6fdd595b0facf
ali <ali.farahat78@gmail.com>
2021-05-23 09:20:51 +0430

    add space to test

--- ChatHouse/app/src/test/java/com/example/chathouse/Pages/HomePageTest1.java
+++ ChatHouse/app/src/test/java/com/example/chathouse/Pages/HomePageTest1.java
@@ -7,6 +7,7 @@ import org.junit.runners.JUnit4;
 import static org.junit.Assert.*;
 
 public class HomePageTest1 {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
